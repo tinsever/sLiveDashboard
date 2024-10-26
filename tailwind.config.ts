28d391051634699d36645edb19e32c7b867a94a8
Tin Sever <web@sevetin.net>
2024-10-26 16:40:34 +0200

    Added basier font. Removed DM Sans.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,22 +1,20 @@
-import aspectRatio from '@tailwindcss/aspect-ratio';
-import forms from '@tailwindcss/forms';
-import typography from '@tailwindcss/typography';
-import type { Config } from 'tailwindcss';
+import aspectRatio from "@tailwindcss/aspect-ratio";
+import forms from "@tailwindcss/forms";
+import typography from "@tailwindcss/typography";
+import type { Config } from "tailwindcss";
 
 export default {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
-
+  content: ["./src/**/*.{html,js,svelte,ts}"],
   theme: {
     extend: {
       colors: {
-        'kai': '#0f0e14',
-        'sl': '#6365ea',
+        kai: "#0f0e14",
+        sl: "#6365ea",
       },
       fontFamily: {
-        'sans': ['"DM Sans"', 'sans-serif']
+        sans: ['"Basier Circle"', "sans-serif"],
       },
     },
   },
-
-  plugins: [typography, forms, aspectRatio]
+  plugins: [typography, forms, aspectRatio],
 } as Config;
